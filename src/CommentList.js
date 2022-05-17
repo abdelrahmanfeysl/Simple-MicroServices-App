04e795281e823884f747ef5c4e21f6bf39110fbc
Abdelrahman Feysl <bidofeysl@gmail.com>
2022-05-17 18:15:49 +0200

    Initial Commit

---
+++ src/CommentList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const CommentList = ({comments}) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
+    if(comment.status === 'approved'){
+      content=comment.content;
+    }
+    if(comment.status === 'pending'){
+      content='this comment is awaiting moderation';
+    }
+    if(comment.status === 'rejected'){
+      content='this comment has been rejected';
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
